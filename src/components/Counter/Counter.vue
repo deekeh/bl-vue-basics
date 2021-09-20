cf374c2ddd5ee973142c8104d6fed60133726919
DK <darshankhot@programmer.net>
2021-09-20 17:56:57 +0530

    [darshan] added styling to counter component

--- src/components/Counter/Counter.vue
+++ src/components/Counter/Counter.vue
@@ -1,7 +1,10 @@
 <template>
   <section class="counter">
-    <span class="count" :v-html="count">{{count}}</span>
+    <span class="count">{{ count }}</span>
+    <!-- <div class="toggles"> -->
     <button class="incrementor" @click="increment()">+</button>
+    <button class="incrementor" @click="count--">-</button>
+    <!-- </div> -->
   </section>
 </template>
 
